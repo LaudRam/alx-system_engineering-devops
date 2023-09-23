93f0672eabc4bcfdf2367230e157489b342f11e3
Laudicia Ramasenya <122815337+LaudRam@users.noreply.github.com>
2023-09-23 19:31:38 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-'''
-    Extends Python script from "0-gather_data_from_an_API.py"
-    to export data in JSON format
-'''
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    my_user_url = 'https://jsonplaceholder.typicode.com/users/' + employee_id
-    todo = 'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id
-    employee_response = requests.get(my_user_url)
-    employee_data = employee_response.json()
-    employee_data = dict(employee_data)
-    name = employee_data.get('username')
-    todo_response = requests.get(todo)
-    todo_data = todo_response.json()
-    the_list = []
-    final_dict = {}
-    for i in todo_data:
-        my_dict = {}
-        for k, v in i.items():
-            if k != 'userId' and k != 'id':
-                my_dict[k] = v
-                my_dict['name'] = name
-                the_list.append(my_dict)
-    final_dict[employee_id] = the_list
-    my_file = employee_id + '.json'
-    with open(my_file, 'w') as f:
-        json_obj = json.dumps(final_dict)
-        f.write(json_obj)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
